cad7942fd72f156b231c47ca0524cb4bc65cbf62
Ryan Werner <rmwerner@umich.edu>
2020-05-14 01:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"]
-var destructivelyAppendKitten = (name) => {return kittens.push(name)} //define your array here
+var destructivelyAppendKitten = (name) => {return kittens.push(name)}
+var destructivelyPrependKitten = (name) => {return kittens.unshift(name)}
+ //define your array here
 
 // Add your functions and code here
