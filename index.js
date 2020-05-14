049d277878718b4ab71999d75e2a6761cf144392
Ryan Werner <rmwerner@umich.edu>
2020-05-14 01:07:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var kittens = ["Milo", "Otis", "Garfield"]
 var destructivelyAppendKitten = (name) => {return kittens.push(name)}
 var destructivelyPrependKitten = (name) => {return kittens.unshift(name)}
 var destructivelyRemoveLastKitten = () => {kittens.pop()}
-var destructivelyRemoveFirstKitten = () => {kittens.unshift()}
+var destructivelyRemoveFirstKitten = () => {return kittens.shift()}
 
 
 
